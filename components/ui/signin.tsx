aacea5bcc314f9a5604e8a36e88303a5c2489d3f
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-03-24 19:04:25 +0530

    fix the Image tag issue

--- components/ui/signin.tsx
+++ components/ui/signin.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
+// import Image from "next/image";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
 import { signIn } from "next-auth/react";
