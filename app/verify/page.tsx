9d41ac316dc4a27f67baaae65abcbaba0ffd18c5
shravanchinchkar <shravanchinchkarstar@gmail.com>
2025-03-24 11:09:25 +0530

    fix the useSearchParams issue

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,17 +1,7 @@
 import { VerifyCodePage } from "@/components/ui/verify-code-page";
-// import { Suspense } from "react";
+
+export const dynamic = 'force-dynamic'
 
 export default function VerifyYourEmail() {
-  return (
-    // <Suspense
-    //   fallback={
-    //     <div className="bg-gradient-to-b from-blue-50 to-white w-screen h-screen flex justify-center items-center">
-    //       Loading...
-    //     </div>
-    //   }
-    // >
-    //   <VerifyCodePage />
-    // </Suspense>
-    <VerifyCodePage/>
-  );
+  return <VerifyCodePage />;
 }
